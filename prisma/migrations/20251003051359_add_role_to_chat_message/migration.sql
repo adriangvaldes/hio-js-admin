64630938ceeadb14ed6b3a7025b5420d36230886
Adrian Valdes <adriangvaldes@gmail.com>
2025-10-03 02:14:23 -0300

    feat: update ChatMessage migration to change sender type from String to Role

---
+++ prisma/migrations/20251003051359_add_role_to_chat_message/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Changed the type of `sender` on the `ChatMessage` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- AlterTable
+ALTER TABLE "ChatMessage" DROP COLUMN "sender",
+ADD COLUMN     "sender" "Role" NOT NULL;
