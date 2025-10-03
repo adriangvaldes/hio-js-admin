77a56b031a927c532f6647f02b04b5885fcf3728
Adrian Valdes <adriangvaldes@gmail.com>
2025-10-03 20:07:11 -0300

    feat: implement client-side form with validation and UI components

---
+++ src/app/login/formClientSide.tsx
@@ -0,0 +1,45 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, useForm } from "react-hook-form";
+import z from "zod";
+import { loginSchema } from "./loginSchema";
+import { FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export function FormClientSide() {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof loginSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder='email' {...field} />
+              </FormControl>
+              <FormDescription>This is your public display name.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,18 @@
+export default function Login() {
+  return (
+    <div className='font-sans flex items-center justify-between min-h-screen bg-white'>
+      <section className='bg-zinc-800 w-full h-screen rounded-r-4xl pl-10 pt-10'>
+        <p className='text-white'>
+          Welcome to <span className='font-bold'>hIO-js</span> Admin
+        </p>
+        <h1 className='text-white font-bold text-3xl max-w-2xl mt-5'>The place where productivity meets simplicity</h1>
+      </section>
+      <section className='bg-white w-full h-screen flex justify-center items-center p-8 rounded-lg shadow-lg'>
+        <div>
+          <h1 className='text-4xl font-bold mb-2'>Get Started Now</h1>
+          <p className='text-slate-400'>Please log in with your credentials to continue</p>
+        </div>
+      </section>
+    </div>
+  );
+}
