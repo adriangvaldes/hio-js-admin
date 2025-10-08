fbe172016756fa98666d1ac8f731219de90127b7
Adrian Valdes <adriangvaldes@gmail.com>
2025-10-08 01:52:14 -0300

    feat: add dashboard layout component and middleware for route handling

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
+
+const publicRoutes = ["/login", "/register"];
+
+const REDIRECT_WHEN_NOT_AUTHENTICATED = "/login";
+
+export function middleware(request: NextRequest) {
+  console.log("Middleware is running");
+  return NextResponse.next();
+}
+
+export const config: MiddlewareConfig = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
